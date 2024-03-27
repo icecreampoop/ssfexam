4a68b0d91d7a63dc59e9d691f6d72503166b1d3d
icecreampoop <pchiku@hotmail.sg>
2024-03-27 12:20:02 +0800

    task 6 done, need check task 7

--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/LoginController.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/controller/LoginController.java
@@ -1,34 +1,49 @@
-// package sg.edu.nus.iss.ibfb4ssfassessment.controller;
+package sg.edu.nus.iss.ibfb4ssfassessment.controller;
 
-// import org.springframework.ui.Model;
-// import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
-// import jakarta.servlet.http.HttpSession;
-// import jakarta.validation.Valid;
-// import sg.edu.nus.iss.ibfb4ssfassessment.model.Login;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import sg.edu.nus.iss.ibfb4ssfassessment.model.Login;
 
-// import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// public class LoginController {
+@Controller
+@RequestMapping
+public class LoginController {
     
+    // TODO: Task 6
+    @GetMapping(path = {"/", "/index", "/login"})
+    public String login(Model model) {
 
+        Login userLogin = new Login();
+        model.addAttribute("userLogin", userLogin);
 
-//     // TODO: Task 6
-//     public String login() {
+        return "view0";
+    }
 
-//     }
+    // TODO: Task 7
+    @PostMapping("/login")
+    public String processlogin(@Valid @ModelAttribute("userLogin") Login userLogin, BindingResult result,
+    HttpSession session, Model model ) {
 
-//     // TODO: Task 7
-//     public String processlogin() {
+        if (result.hasErrors()) {
+            return "view0";
+        }
         
-
-//     }
+        return "view1";
+    }
     
 
-//     // For the logout button shown on View 2
-//     // On logout, session should be cleared
-//     public String logout() {
+    // // TODO For the logout button shown on View 2
+    // // On logout, session should be cleared
+    // public String logout() {
 
-//     }
+    // }
     
-// }
+}
--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
@@ -1,8 +1,41 @@
 package sg.edu.nus.iss.ibfb4ssfassessment.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 public class Login {
 
+    @NotNull (message = "Please enter email")
+    @NotEmpty (message = "Please enter email")
+    @Email(message = "Please enter a valid email :(")
+    private String email;
 
+    //format yyyy-mm-dd, cannot be current or future date
+    @NotNull (message = "Please enter Date Of Birth")
+    @Past (message = "Are you from the future?")
+    @DateTimeFormat (pattern = "yyyy-MM-dd")
+    private Date birthDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
     
 }
